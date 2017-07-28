66ffff25cce416c39f6a0f7af605cf950586a5fe
Eric Eggert <mail@yatil.net>
2017-07-28 15:12:41 +0200

    Fractal add check-errors

--- fractal.js
+++ fractal.js
@@ -36,3 +36,17 @@ fractal.web.set('static.path', path.join(__dirname, 'public'));
 
 /* Set the static HTML build destination */
 fractal.web.set('builder.dest', path.join(__dirname, 'docs'));
+
+fractal.cli.command('check-errors', function(opts, done){
+    process.on('unhandledRejection', () => {}); // workaround for poor error catching in the views!
+    fractal.load().then(src => {
+        for (let comp of fractal.components.flatten()) {
+            comp.render().then(html => {
+                this.console.success(`@${comp.handle}`);
+            }).catch(err => {
+                this.console.error(`@${comp.handle} - ${err.message} (${comp.viewPath})`);
+            });
+        }
+    });
+    done();
+});
