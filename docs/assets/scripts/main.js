02dae902f0cc0cccf11751651435fcf1d0d752a6
Eric Eggert <mail@yatil.net>
2017-09-20 16:41:44 +0200

    Headings

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,47 +1,119 @@
-/* var menutimer;
+(function () {
+  'use strict';
 
-function forEach(elements, func) {
-  Array.prototype.forEach.call(elements, func);
-}
+  var addclass = function(el, className) {
+    if (el.classList)
+      el.classList.add(className);
+    else
+      el.className += ' ' + className;
+  };
 
-function showMenu(target) {
-  console.log(target);
-  var submenus = document.querySelectorAll('.mainsub ul');
-  forEach(submenus, function(e) {
-    console.log(e);
-    e.classList.remove('active');
-  })
-  var targetMenu = target.getAttribute('data-submenu');
-  console.log(targetMenu);
-  var submenu = document.getElementById(targetMenu);
-  submenu.classList.add('active');
-}
+  var remclass = function(el, className) {
+    if (el.classList)
+      el.classList.remove(className);
+    else
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  };
 
-function hideMenu(target) {
-  var submenus = document.querySelectorAll('.mainsub ul');
-  forEach(submenus, function(e) {
-    e.classList.remove('active');
-  })
-}
+  var hasclass = function(el, className) {
+    if (el.classList)
+      return el.classList.contains(className);
+    else
+      return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+  };
 
-var mainnavitems = document.querySelectorAll('.mainnav a');
+  var spc = document.createTextNode(' ');
 
-forEach(mainnavitems, function(e){
-  e.addEventListener('mouseenter', function(event) {showMenu(event.target); clearTimeout(menutimer);});
-  e.addEventListener('focus', function(event) {showMenu(event)});
-  e.addEventListener('mouseleave', function(event) {
-    menutimer = setTimeout(function(event){
-      hideMenu(event)
-    }, 1000);});
-  e.addEventListener('blur', function(event) {hideMenu(event)});
-});
+  var headings = document.querySelectorAll('.content h2[id], .content h3[id], .content h4[id]');
+  var firstheading = headings[0];
 
-var subnavitems = document.querySelectorAll('.mainsub ul');
-forEach(subnavitems, function(e){
-  e.addEventListener('mouseenter', function(event) {clearTimeout(menutimer);});
-  e.addEventListener('mouseleave', function(event) {
-    menutimer = setTimeout(function(event){
-      hideMenu(event)
-    }, 1000);});
-});
-*/
+  if (firstheading) {
+    addclass(firstheading, 'first');
+
+    var toc_elements = headings; // $('.content h2[id], .ap')
+
+    var excount = 1,
+        apcount = 1;
+
+    var toc_outer = document.createElement('figure');
+    toc_outer.setAttribute('role', 'navigation');
+    toc_outer.setAttribute('aria-describedby', 'toc_desc');
+    toc_outer.innerHTML = '<figcaption id="toc_desc">On this page <a href="#skipotp" class="visuallyhidden focusable">(skip)</a></figcaption><div class="figcontent"></div>';
+    if (toc_outer.classList)
+      toc_outer.classList.add('toc');
+    else
+      toc_outer.className += ' ' + ('toc');
+    var toc_wrap = document.createElement('ul');
+    var toc_elem = document.createElement('li');
+    var nesting = false;
+    var subnesting = false;
+    var sub_wrap, sub_sub_wrap, last_elem, last_sub_elem;
+    Array.prototype.forEach.call(toc_elements, function(el, i){ // … .each(…)
+      // console.log(el.localName + ': ' + el.textContent + ' // ' + nesting);
+      var cur_elem = toc_elem.cloneNode(true);
+      if ((el.localName==="h4") && (subnesting===false)) {
+        sub_sub_wrap = toc_wrap.cloneNode(false);
+      }
+      if ((el.localName==="h3") && (nesting===false)) {
+        sub_wrap = toc_wrap.cloneNode(false);
+      }
+      if ((el.localName==="h3") && (subnesting===true)) {
+        last_sub_elem.appendChild(sub_sub_wrap);
+        subnesting = false;
+      }
+      if ((el.localName==="h2") && (nesting===true)) {
+        if (subnesting===true) {
+          last_sub_elem.appendChild(sub_sub_wrap);
+          subnesting = false;
+        }
+        last_elem.appendChild(sub_wrap);
+        nesting = false;
+      }
+
+      if (hasclass(el,"ex")) {
+        el.insertAdjacentHTML('afterbegin', "<b>Example " + excount + ":</b> ");
+        excount++;
+        if (!hasclass(el, "inap")) {
+          apcount = 1;
+        }
+      }
+      if (hasclass(el,"ap")) {
+        el.insertAdjacentHTML('afterbegin', "<b>Approach " + apcount + ":</b> ");
+        apcount++;
+      }
+      if (hasclass(el,"newex")) {
+        excount = 1;
+      }
+      if (hasclass(el,"newap")) {
+        apcount = 1;
+      }
+
+      cur_elem.innerHTML = '<a class="' + el.getAttribute('class') + '" href="#' + el.getAttribute('id') + '">' + el.innerHTML + '</a>';
+
+       console.log(cur_elem);
+      if (el.localName==="h4") {
+        sub_sub_wrap.appendChild(cur_elem);
+        subnesting = true;
+      } else if (el.localName==="h3") {
+        sub_wrap.appendChild(cur_elem);
+        nesting = true;
+        last_sub_elem = cur_elem;
+      } else {
+        toc_wrap.appendChild(cur_elem);
+        last_elem = cur_elem;
+      }
+    });
+
+    if (nesting===true) {
+      last_elem.appendChild(sub_wrap);
+      nesting = false;
+    }
+
+    toc_outer.querySelectorAll('.figcontent')[0].innerHTML = toc_wrap.outerHTML;
+
+    var inner = document.querySelectorAll('.inner > :first-child')[0];
+    inner.insertAdjacentHTML('beforebegin', toc_outer.outerHTML + '<span id="skipotp" class="visuallyhidden"></span>');
+
+  }
+
+}());
