ceb895ed04b66f8dddf208162b02a393c0ef8c5e
Eric Eggert <mail@yatil.net>
2017-09-26 14:57:27 +0200

    Add keyboard navigation

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -130,4 +130,18 @@
     });
   });
 
+  Array.prototype.forEach.call(menuItems, function(el, i){
+    el.querySelector('a').addEventListener("click",  function(event){
+      if (this.parentNode.className == "has-submenu") {
+        this.parentNode.className = "has-submenu open";
+        this.setAttribute('aria-expanded', "true");
+      } else {
+        this.parentNode.className = "has-submenu";
+        this.setAttribute('aria-expanded', "false");
+      }
+      event.preventDefault();
+      return false;
+    });
+  });
+
 }());
