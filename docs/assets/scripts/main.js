b6ef027b1d4c63759409f6fdf9112aa8670fb912
Eric Eggert <mail@yatil.net>
2017-09-28 13:57:50 +0200

    clearfix

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -134,16 +134,18 @@
   });
 
   Array.prototype.forEach.call(menuItems, function(el, i){
-    el.querySelector('a').addEventListener("click",  function(event){
-      if (this.parentNode.className == "has-submenu") {
-        this.parentNode.className = "has-submenu open";
-        this.setAttribute('aria-expanded', "true");
-      } else {
-        this.parentNode.className = "has-submenu";
-        this.setAttribute('aria-expanded', "false");
+    el.querySelector('a').addEventListener("keyup",  function(event){
+      if (event.keyCode == 13) {
+        if (this.parentNode.className == "has-submenu") {
+          this.parentNode.className = "has-submenu open";
+          this.setAttribute('aria-expanded', "true");
+        } else {
+          this.parentNode.className = "has-submenu";
+          this.setAttribute('aria-expanded', "false");
+        }
+        event.preventDefault();
+        return false;
       }
-      event.preventDefault();
-      return false;
     });
   });
 
