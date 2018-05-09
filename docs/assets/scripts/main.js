1bd8266aa5876c49f2da30a5e97c80083215735c
Eric Eggert <mail@yatil.net>
2018-05-09 12:49:16 +0200

    Test if button is big in relation to viewport and have it inline if so
    Fixes https://github.com/w3c/wai-website/issues/71

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -130,8 +130,14 @@
     if (!ticking) {
 
       window.requestAnimationFrame(function() {
-        if (last_known_scroll_position > (window.innerHeight * 0.66)) {
+        if (last_known_scroll_position > (window.innerHeight * 0.75)) {
           addclass(document.querySelector('.button-backtotop'), 'active');
+          if ((window.innerHeight/document.querySelector('.button-backtotop').clientHeight) < 6) {
+            // If the back to top button is higher than 20% of the available viewport size, add a inline class
+            addclass(document.querySelector('.button-backtotop'), 'inline');
+          } else {
+            remclass(document.querySelector('.button-backtotop'), 'inline');
+          }
         } else {
           if (last_known_scroll_position < 1) {
             remclass(document.querySelector('.button-backtotop'), 'active');
