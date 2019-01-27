155f312387977987f80ac2827f3f2264d6c8da5b
Eric Eggert <mail@yatil.net>
2019-01-27 10:19:05 +0100

    Move from CSS-Next to Preset Env
    Signed-off-by: Eric Eggert <mail@yatil.net>

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,33 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
-const postcssImport = require('postcss-import');
-const cssnext = require('postcss-cssnext');
+const atImport = require('postcss-easy-import');
+const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
-const importer = require('postcss-easy-import');
-const autoprefixer = require('autoprefixer');
+var pixrem = require('gulp-pixrem');
 
-var csssource = "css/style.css";
+const csssource = "css/style.css";
 
 const processors = [
-  importer({
-    glob: true,
-  }),
-  cssnext({features: { autoprefixer: false }}),
-  autoprefixer({grid:false})
+  atImport({glob: true}),
+  postcssPresetEnv({
+    stage: 0,
+    features: {
+      'nesting-rules': true,
+      'custom-media-queries': true,
+      'media-query-ranges': true,
+      'custom-selectors': true
+    },
+    autoprefixer: {grid:false},
+    browsers: 'last 2 version, > 1%, IE 10'
+  })
 ];
 
 gulp.task('css', function () {
     return gulp.src(csssource)
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
+        .pipe(pixrem({ rootValue: '16px' }))
         .pipe(cssnano({safe: true}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./public/assets/css/'));
